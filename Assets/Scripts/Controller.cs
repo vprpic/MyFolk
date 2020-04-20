5421d4e8ebed7afe9d35311d6805b2de0a44deeb
Vinka Prpic <maggie222222@gmail.com>
2020-04-20 08:49:18 +0200

    Added NavMesh navigation and the UltEvent Plugin

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Controller : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(ray, out hitInfo, 100))
+            {
+                agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
