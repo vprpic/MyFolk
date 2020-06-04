13d1304c8dd149301151e4eec241008d6bf082b6
Vinka Prpic <maggie222222@gmail.com>
2020-06-04 13:44:40 +0200

    Probuilder added

---
+++ Assets/Scripts/BuildMode/ProBuilderWallCreator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProBuilderWallCreator
+{
+	public float xSize, ySize, zSize;
+	int xInt, yInt, zInt;
+
+
+}
